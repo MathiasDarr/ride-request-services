6967e5169baa69c631d716888468d74fa87473fe
mddarr <mddarr@gmail.com>
2020-12-30 02:04:32 -0800

    Add a request producer class

---
+++ rideshare_microservices/ride-services/event-producer/src/main/java/org/mddarr/producer/EventProducer.java
@@ -0,0 +1,8 @@
+package org.mddarr.producer;
+
+public class EventProducer {
+
+    public static void main(String[] args) {
+        System.out.println("PRODUCER");
+    }
+}
---
+++ rideshare_microservices/ride-services/ride-request-service/src/main/java/org/mddarr/rides/request/service/producers/RideRequestProducer.java
@@ -0,0 +1,9 @@
+package org.mddarr.rides.request.service.producers;
+
+public class RideRequestProducer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
