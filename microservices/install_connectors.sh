085f4c56306b8c93e460dfbb45c3f26e9c89fe4c
mddarr <mddarr@gmail.com>
2020-12-31 04:38:32 -0800

    an offsets table is getting created..

--- microservices/install_connectors.sh
+++ microservices/install_connectors.sh
@@ -2,5 +2,5 @@
 docker exec connect confluent-hub install  debezium/debezium-connector-postgresql:0.9.4 --no-prompt
 docker exec connect confluent-hub install confluentinc/kafka-connect-cassandra:latest --no-prompt
 docker restart connect
-sleep 30
-bash post.sh connectors/cassandra_coordinates_sink_connector.json
+sleep 15
+#bash post.sh connectors/cassandra_coordinates_sink_connector.json
