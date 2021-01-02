b2038fce8842f7af1a8fb203171315b14d3303bd
mddarr <mddarr@gmail.com>
2021-01-02 01:29:37 -0800

    seed the drivers table ..

--- data_model/generate_data.py
+++ data_model/generate_data.py
@@ -15,28 +15,62 @@ fake = Faker()
 fake.random.seed(4321)
 
 
-keys = ['userid', 'first_name', 'last_name', 'email','password','phone_number', 'city']
-cities = ['Seattle', 'Tacoma', 'Bellevue', 'Everett']
-cities_to_area_code_map = {'Seattle':'206','Tacoma':'253', 'Bellevue':'425', 'Everett':'425'}
+
+def generate_drivers_data():
+    keys = ['driverid', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'city']
+    cities = ['Seattle', 'Tacoma', 'Bellevue', 'Everett']
+    cities_to_area_code_map = {'Seattle': '206', 'Tacoma': '253', 'Bellevue': '425', 'Everett': '425'}
+
+    with open('data/drivers/drivers.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+
+        users = []
+
+        for i in range(20000):
+
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            email = first_name + last_name + '@gmail.com'
+            password = '1!ZionTF'
+            city = random.choice(cities)
+            area_code = cities_to_area_code_map[city]
+            phone_number = area_code + '-' + ''.join(random.choice('123456789') for _ in range(3)) + '-' + ''.join(
+                random.choice('123456789') for _ in range(3))
+            user = {'driverid': str(uuid.uuid4()), 'first_name': first_name, 'last_name': last_name, 'email': email,
+                    'password': password, 'city': city, 'phone_number': phone_number}
+            users.append(user)
+        dict_writer.writerows(users)
 
 
-with open('data/users/users.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-
-    users = []
-
-    for i in range(200000):
-        userid = str(uuid.uuid4())
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        email = first_name + last_name + '@gmail.com'
-        password = '1!ZionTF'
-        city = random.choice(cities)
-        area_code = cities_to_area_code_map[city]
-        phone_number = area_code +'-' + ''.join(random.choice('123456789') for _ in range(3)) + '-' + ''.join(random.choice('123456789') for _ in range(3))
-        user = {'userid' : str(uuid.uuid4()), 'first_name':first_name, 'last_name':last_name, 'email':email, 'password':password, 'city':city, 'phone_number':phone_number}
-        users.append(user)
-    dict_writer.writerows(users)
 
 
+def generate_users_data():
+
+    keys = ['userid', 'first_name', 'last_name', 'email','password','phone_number', 'city']
+    cities = ['Seattle', 'Tacoma', 'Bellevue', 'Everett']
+    cities_to_area_code_map = {'Seattle':'206','Tacoma':'253', 'Bellevue':'425', 'Everett':'425'}
+
+
+    with open('data/users/users.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+
+        users = []
+
+        for i in range(200000):
+            userid = str(uuid.uuid4())
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            email = first_name + last_name + '@gmail.com'
+            password = '1!ZionTF'
+            city = random.choice(cities)
+            area_code = cities_to_area_code_map[city]
+            phone_number = area_code +'-' + ''.join(random.choice('123456789') for _ in range(3)) + '-' + ''.join(random.choice('123456789') for _ in range(3))
+            user = {'userid' : str(uuid.uuid4()), 'first_name':first_name, 'last_name':last_name, 'email':email, 'password':password, 'city':city, 'phone_number':phone_number}
+            users.append(user)
+        dict_writer.writerows(users)
+
+
+if __name__=='__main__':
+    generate_drivers_data()
--- data_model/seed_postgres_data.py
+++ data_model/seed_postgres_data.py
@@ -37,23 +37,25 @@ def populate_users_table():
 def populate_drivers_table():
     create_drivers_table = """
             CREATE TABLE drivers (
-                    driver_id VARCHAR(50) PRIMARY KEY,
-                    first_name VARCHAR(30),
-                    last_name VARCHAR(30),
-                    email VARCHAR(30), 
-                    password VARCHAR(30)
+                    driverid VARCHAR(50) PRIMARY KEY,
+                    first_name VARCHAR(50),
+                    last_name VARCHAR(50),
+                    email VARCHAR(50), 
+                    password VARCHAR(50),
+                    phone_number VARCHAR(50),
+                    city VARCHAR(50)
             );
     """
     cur.execute(create_drivers_table)
     conn.commit()
     DRIVERS_CSV_FILE = 'data/drivers/drivers.csv'
-    insert_into_drivers_table = """INSERT INTO drivers(driver_id, first_name, last_name, email, password) VALUES(%s,%s,%s,%s,%s);"""
+    insert_into_drivers_table = """INSERT INTO drivers(driverid, first_name, last_name, email, password, phone_number, city) VALUES(%s,%s,%s,%s,%s, %s, %s);"""
 
     with open(DRIVERS_CSV_FILE, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
 
         for row in reader:
-            cur.execute(insert_into_drivers_table, [row['driver_id'], row['first_name'], row["last_name"], row['email'], row['password']])
+            cur.execute(insert_into_drivers_table, [row['driverid'], row['first_name'], row["last_name"], row['email'], row['password'], row['phone_number'], row['city']])
     conn.commit()
 
 
@@ -64,8 +66,8 @@ def populate_rides_table():
                     ride_time timestamp,
                     riders INTEGER,
                     ride_status VARCHAR(30),
-                    driver_id VARCHAR(50) REFERENCES drivers(driver_id),
-                    user_id VARCHAR(50) REFERENCES users(user_id)
+                    driverid VARCHAR(50) REFERENCES drivers(driverid),
+                    userid VARCHAR(50) REFERENCES users(userid)
             );
     """
     cur.execute(create_rides_table)
@@ -84,7 +86,7 @@ def populate_ride_requests_table():
     create_users_table = """
             CREATE TABLE ride_requests (
                     request_id VARCHAR(50) PRIMARY KEY,
-                    user_id VARCHAR(50) REFERENCES users(user_id),
+                    user_id VARCHAR(50) REFERENCES users(userid),
                     request_time VARCHAR(30),
                     riders INTEGER
             );
@@ -92,7 +94,7 @@ def populate_ride_requests_table():
     cur.execute(create_users_table)
     conn.commit()
     USERS_CSV_FILE = 'data/ride_requests/ride_requests.csv'
-    insert_into_users_table = """INSERT INTO ride_requests(request_id, user_id, request_time, riders) VALUES(%s,%s,%s, %s);"""
+    insert_into_users_table = """INSERT INTO ride_requests(request_id, userid, request_time, riders) VALUES(%s,%s,%s, %s);"""
 
     with open(USERS_CSV_FILE, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -104,8 +106,8 @@ def populate_ride_requests_table():
 if __name__ =='__main__':
     conn = psycopg2.connect(host="localhost", port="5432", user="postgres", password="postgres", database="postgresdb")
     cur = conn.cursor()
-    # populate_drivers_table()
-    populate_users_table()
+    populate_drivers_table()
+    #populate_users_table()
     # populate_ride_requests_table()
     # populate_rides_table()
     print("THE POSTGRES DATABASE HAS BEEN SEEDED.")
